cc41917f52c3e4814e09e1cc52f95eff379fe6cd
Horikoshi Daisuke <jcmtjgej@gmail.com>
2021-12-23 19:54:16 +0900

    WIP

--- TestEncrypted.bat
+++ TestEncrypted.bat
@@ -4,11 +4,6 @@ FOR %%i IN (*zip) DO (
     echo garbage | "c:\Program Files\7-Zip\7z.exe" t -y %%~fi >nul 2>nul
 if errorlevel 2 (
    echo File is encrypted
-   echo %%~fi >> encryptedlist.txt
-   exit /b %errorlevel%
-)
-if errorlevel 0 (
-   echo File is NOT encrypted
-   echo %%~fi >> NOTencryptedlist.txt
+   echo %%~i >> encryptedlist.txt
 )
 )
