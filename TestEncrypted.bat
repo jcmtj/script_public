b963712a10ed4ac0900b2582d05d3947b6f038de
Horikoshi Daisuke <jcmtjgej@gmail.com>
2021-12-23 14:03:50 +0900

    Add TestEncrypted.bat

---
+++ TestEncrypted.bat
@@ -0,0 +1,14 @@
+@echo off
+
+FOR %%i IN (*zip) DO (
+    echo garbage | "c:\Program Files\7-Zip\7z.exe" t -y %%~fi >nul 2>nul
+if errorlevel 2 (
+   echo File is encrypted
+   echo %%~fi >> encryptedlist.txt
+   exit /b %errorlevel%
+)
+if errorlevel 0 (
+   echo File is NOT encrypted
+   echo %%~fi >> NOTencryptedlist.txt
+)
+)
